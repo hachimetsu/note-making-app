3d45c51119df38d11a07a40197dc88bc8a54a127
Hachi Metsu <hachimetsu@gmail.com>
2023-04-06 15:52:15 +0000

    shift to local mach..

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,8 +5,21 @@ function App() {
 
     return (
         <div id="workspace" className="flex">
-            <div id="sidebar" ref={DynamicDiv} className="h-screen flex bg-teal-900" style={{ width: Xdrag }} >
-                <div className="content w-full"> sidebar-Width: {Xdrag} </div>
+            <div id="sidebar" ref={DynamicDiv} className="h-screen flex bg-teal-900 max-w-sm" style={{ width: Xdrag, minWidth: "200px" }} >
+                <div className="content w-full"> 
+                    {/* <span>sidebar-Width: {Xdrag}</span> */}
+                    <div className="userProfile">Hachimetsu ;&lt;</div>
+                    <div>
+                        <div className="Search">Serch here</div>
+                        <div className="Setting">setting option for workspace</div>
+                    </div>
+                    <div className="Index">
+                        <div className="Favorate">Favorate + </div>
+                        <div className="Private">Private + </div>
+                        <div className="All">ALL</div>
+                    </div>
+                    <div className="NewPage">New Page +</div>
+                </div>
                 <div id="dragLine" ref={DragDiv} className=" border-r-2 border-red-700 h-full shadow-lg hover:cursor-col-resize px-1" />
             </div>
             <div className="main" />
